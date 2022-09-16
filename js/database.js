25fccec900181a334f4623862109129f6e887138
Ethmane Didi Bilal <Ethmane.Bilal@stud.noroff.no>
2022-09-16 12:23:04 +0200

    Lot of change has been made

---
+++ js/database.js
@@ -0,0 +1,329 @@
+var postDataArray = [
+  {
+    id: 1,
+    title: "enjoy the good food",
+    imgUrl: "/images/blogfood6.jpg",
+    desc_first:
+      "Food is a very important part of everyone's life. If you want to be healthy, you have to eat healthy. One of the easiest ways to do that is to keep your cooking nice and simple.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 2,
+    title: "enjoy the good food",
+    imgUrl: "/images/blogfood2.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 3,
+    title: "enjoy the good food",
+    imgUrl: "/images/blogfood3.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 4,
+    title: "enjoy the good food",
+    imgUrl: "/images/blogfood4.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 5,
+    title: "enjoy the good food",
+    imgUrl: "/images/blogfood5.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 6,
+    title: "enjoy the good food",
+    imgUrl: "/images/blogfood7.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 7,
+    title: "enjoy the good food",
+    imgUrl: "/images/blogfood8.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 8,
+    title: "enjoy the good food",
+    imgUrl: "/images/blogfood9.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 9,
+    title: "enjoy the good food",
+    imgUrl: "/images/blogfood10.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 10,
+    title: "Noodles chicken salad",
+    imgUrl: "/images/blogfood10.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 11,
+    title: "Noodles chicken salad",
+    imgUrl: "/images/blogfood2.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 12,
+    title: "Noodles chicken salad",
+    imgUrl: "/images/blogfood1.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 13,
+    title: "Noodles chicken salad",
+    imgUrl: "/images/blogfood9.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 14,
+    title: "Noodles chicken salad",
+    imgUrl: "/images/blogfood1.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 15,
+    title: "Noodles chicken salad",
+    imgUrl: "/images/blogfood8.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 16,
+    title: "Noodles chicken salad",
+    imgUrl: "/images/blogfood6.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 17,
+    title: "Noodles chicken salad",
+    imgUrl: "/images/blogfood5.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 18,
+    title: "Noodles chicken salad",
+    imgUrl: "/images/blogfood11.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 19,
+    title: "Noodles chicken salad",
+    imgUrl: "/images/blogfood3.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+  {
+    id: 20,
+    title: "Noodles chicken salad",
+    imgUrl: "/images/blogfood4.jpg",
+    desc_first:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    desc_second:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+    quote:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. perferendis soluta.Lorem, ipsum dolor sit ametco.",
+  },
+];
+
+// here is post page data
+
+//<button id="didi_view_more_btn" onclick="viewMoreBtn()">View More</button>
+var showItemCount = 10;
+var showViewMoreBtn = false;
+if (document.getElementById("post-data-section")) {
+  addData();
+}
+function addData() {
+  var dataStr = "";
+  postDataArray.forEach((element, index) => {
+    if (index < showItemCount) {
+      dataStr += `<figure>
+<h3>${element.title}</h3>
+<img src="${element.imgUrl}"  alt="food" display: grid; height="210" width="300"   transform: scale(1.05);  />
+</figure>
+<p>${element.desc_first}
+<a style="display:block;padding:3px 5px 1px 10px; cursor: pointer; background: #a52a2a;width:80px;color:#ffffff;" href="./post_specific.html?id=${element.id}">Read </a>
+</p>`;
+    }
+    if (index >= showItemCount) {
+      showViewMoreBtn = true;
+    } else {
+      showViewMoreBtn = false;
+    }
+  });
+
+  document.getElementById("post-data-section").innerHTML = dataStr;
+  if (showViewMoreBtn) {
+    document.getElementById("didi_view_more_btn").style.display = "block";
+  } else {
+    document.getElementById("didi_view_more_btn").style.display = "none";
+  }
+  //<blockquote class="quote">
+}
+function viewMoreBtn() {
+  showItemCount += 10;
+  addData();
+}
+// <p><span><i class="ri-double-quotes-l"></i></span>${element.q}<span ><i class="ri-double-quotes-r"></i></span></p> </blockquote>
+//<p>${element.desc_second}</p>
+
+// post specific
+
+const params = new URLSearchParams(window.location.search);
+for (const param of params) {
+  console.log(param);
+}
+
+console.log("test", params.get("id"));
+const id = +params.get("id");
+if (id) {
+  var currentPost = postDataArray.find((x) => x.id === id);
+
+  console.log(currentPost);
+  var dataStr = "";
+  dataStr += `
+           <div>
+             <figure>
+        <h1 style="text-align:center;text-transform:uppercase;box-shadow: 0 4px 3px -2px gray;margin-top:10px;">${postDataArray[0].title}</h1>
+        <a onclick="modelImgClick()"> <img src="${currentPost.imgUrl}" height="400px" width="360px"  alt="food" class="" /></a>
+       </figure>
+       <p>${currentPost.desc_first}
+        <blockquote class="quote">
+      <p><span><i class="ri-double-quotes-l"></i></span>${currentPost.quote}<span ><i class="ri-double-quotes-r"></i></span></p> </blockquote>
+      ${currentPost.desc_second}</p>
+      <p>${currentPost.desc_first}</p>
+      
+    </p>
+
+    <div id="didi_modal_image_id" class="didi_modal_image">
+      <div style="width:760px;">
+      <button style="font-size:28px;padding:5px;background:tomato;color:#ffffff;float:right;transform:translateY(50px)" onclick="modalClose()">X</button>
+      </div>
+       <img src="${currentPost.imgUrl}" height="700px" width="760px" />
+      </div>
+   
+    </div>`;
+  document.getElementById("post-data-section-post-specific").innerHTML =
+    dataStr;
+  setTimeout(() => {
+    document.getElementsByClassName("loader-container")[0].style.display =
+      "none";
+  }, 1000);
+}
+
+function modalClose() {
+  document
+    .getElementById("didi_modal_image_id")
+    .classList.add("didi_modal_image");
+  document
+    .getElementById("didi_modal_image_id")
+    .classList.remove("didi_modal_image_show");
+}
+
+function modelImgClick() {
+  //console.log(document.getElementById("didi_model_image_id").classList.toggle("didi_modal_image"))
+  document
+    .getElementById("didi_modal_image_id")
+    .classList.remove("didi_modal_image");
+  document
+    .getElementById("didi_modal_image_id")
+    .classList.add("didi_modal_image_show");
+}
+
+//
