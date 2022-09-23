dd029894d5ec69efdba0a1d10d72801f2124ac43
Ethmane Didi Bilal <Ethmane.Bilal@stud.noroff.no>
2022-09-23 10:22:53 +0200

    some image has been deleted

--- js/app.js
+++ js/app.js
@@ -73,20 +73,16 @@ const swiper = new Swiper(".swiper", {
   },
 });
 
+// filter Search
 $("#filter").keyup(function () {
-  // Retrieve the input field text and reset the count to zero
   var filter = $(this).val(),
     count = 0;
 
-  // Loop through the comment list
   $("#posts div .article-title").each(function () {
-    // If the list item does not contain the text phrase fade it out
     if ($(this).text().search(new RegExp(filter, "i")) < 0) {
-      $(this).hide(); // MY CHANGE
-
-      // Show the list item if the phrase matches and increase the count by 1
+      $(this).hide();
     } else {
-      $(this).show(); // MY CHANGE
+      $(this).show();
       count++;
     }
   });
