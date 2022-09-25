3af19e1e6dbbd149f880b3d9f3a5f9f5b9853073
Ethmane Didi Bilal <Ethmane.Bilal@stud.noroff.no>
2022-09-25 18:34:55 +0200

    new page update

--- js/app.js
+++ js/app.js
@@ -88,6 +88,49 @@ $("#filter").keyup(function () {
   });
 });
 
+$(".form")
+  .find("input, textarea")
+  .on("keyup blur focus", function (e) {
+    var $this = $(this),
+      label = $this.prev("label");
+
+    if (e.type === "keyup") {
+      if ($this.val() === "") {
+        label.removeClass("active highlight");
+      } else {
+        label.addClass("active highlight");
+      }
+    } else if (e.type === "blur") {
+      if ($this.val() === "") {
+        label.removeClass("active highlight");
+      } else {
+        label.removeClass("highlight");
+      }
+    } else if (e.type === "focus") {
+      if ($this.val() === "") {
+        label.removeClass("highlight");
+      } else if ($this.val() !== "") {
+        label.addClass("highlight");
+      }
+    }
+  });
+
+// Global variables
+
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container-column");
+
+signUpButton.addEventListener("click", () =>
+  container.classList.add("right-panel-active")
+);
+
+signInButton.addEventListener("click", () =>
+  container.classList.remove("right-panel-active")
+);
+
+console.log(signInButton);
+
 // open/close search  form popup
 //const formOpenBtn = selectElement("#search-icon");
 //const formCloseBtn = selectElement("#form-close-btm");
