7a3296a40a23a726878df7dd19c716b04b5a03a0
Ethmane Didi Bilal <Ethmane.Bilal@stud.noroff.no>
2022-09-29 22:17:00 +0200

    update changes

--- js/app.js
+++ js/app.js
@@ -115,143 +115,82 @@ $(".form")
     }
   });
 
-// Global variables
+// login form  variables
 
-//sign-in and sign-up switch
-const signUpButton = document.getElementById("signUp");
-const signInButton = document.getElementById("signIn");
-const container = document.getElementById("container_main2");
+function setFormMessage(formElement, type, message) {
+  const messageElement = formElement.querySelector(".form_message");
+  messageElement.textContent = message;
+  messageElement.classList.remove("form_message-success", "form_message-error");
 
-signUpButton.addEventListener("click", () => {
-  container.classList.add("right-panel-active");
-});
+  messageElement.classList.add("form_message--${type}");
+}
 
-signInButton.addEventListener("click", () => {
-  container.classList.remove("right-panel-active");
-});
+function setInputError(inputElement, message) {
+  inputElement.classList.add("form_input--error");
+  inputElement.parentElement.querySelector(
+    ".form_input-error_message"
+  ).textContent = message;
+}
 
-//show and hide password
-const showPassSignUp = document.getElementById("show-password-sign-up");
-const hidePassSignUp = document.getElementById("hide-password-sign-up");
+function clearInputError(inputElement) {
+  inputElement.classList.remove("form_input--error");
+  inputElement.parentElement.querySelector(
+    ".form_input-error_message"
+  ).textContent = "";
+}
 
-const passwordInputSignUp = document.getElementById("password-sign-up");
+//setFormMessage(loginForm, "success", "You´re logged in!");
 
-showPassSignUp.addEventListener("click", () => {
-  passwordInputSignUp.type = "";
-  showPassSignUp.classList.add("hide");
-  hidePassSignUp.classList.remove("hide");
-});
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.querySelector("#login");
+  const createAccountFrom = document.querySelector("#createAccount");
 
-hidePassSignUp.addEventListener("click", () => {
-  passwordInputSignUp.type = "password";
-  hidePassSignUp.classList.add("hide");
-  showPassSignUp.classList.remove("hide");
-});
+  document
+    .querySelector("#linkCreateAccount")
+    .addEventListener("click", (event) => {
+      event.preventDefault();
+      loginForm.classList.add("form--hidden");
+      createAccountFrom.classList.remove("form--hidden");
+    });
 
-const showPassSignIn = document.getElementById("show-password-sign-in");
-const hidePassSignIn = document.getElementById("hide-password-sign-in");
+  document.querySelector("#linkLogin").addEventListener("click", (event) => {
+    event.preventDefault();
+    loginForm.classList.remove("form--hidden");
+    createAccountFrom.classList.add("form--hidden");
+  });
 
-const passwordInputSignIn = document.getElementById("password-sign-in");
+  loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-showPassSignIn.addEventListener("click", () => {
-  passwordInputSignIn.type = "";
-  showPassSignIn.classList.add("hide");
-  hidePassSignIn.classList.remove("hide");
-});
+    // Perform your AJAX/Fetch login
 
-hidePassSignIn.addEventListener("click", () => {
-  passwordInputSignIn.type = "password";
-  hidePassSignIn.classList.add("hide");
-  showPassSignIn.classList.remove("hide");
-});
+    setFormMessage(loginForm, "error", "Invalid username/password combination");
+  });
 
-// sign-up form validation
-const form = document.getElementById("form");
-const username = document.getElementById("name");
-const email = document.getElementById("email");
-const password = document.getElementById("password-sign-up");
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  function checkInputs() {
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-
-    if (usernameValue === "") {
-      setErrorFor(username, "Name cannot be blank");
-    } else {
-      setSuccessFor(username);
-      if (emailValue === "") {
-        setErrorFor(email, "Email cannot be blank");
-      } else if (!isEmail(emailValue)) {
-        setErrorFor(email, "Not a valid email");
-      } else {
-        setSuccessFor(email);
-        if (passwordValue === "") {
-          setErrorFor(password, "Password cannot be blank");
-        } else if (passwordValue.length < 6) {
-          setErrorFor(password, "Password must be at least 6 characters long");
-        } else {
-          setSuccessFor(password);
-          e.currentTarget.submit();
-        }
+  document.querySelectorAll(".form-input").forEach((inputElement) => {
+    inputElement.addEventListener("blur", (e) => {
+      if (
+        e.target.id === "signupUsername" &&
+        e.target.value.length > 0 &&
+        e.target.value.length < 10
+      ) {
+        setInputError(
+          inputElement,
+          "Username must be at least 10 characters in length"
+        );
       }
-    }
-  }
-
-  checkInputs();
+    });
+    inputElement.addEventListener("input", (e) => {
+      clearInputError(inputElement);
+    });
+  });
 });
 
-// sign-in form validation
-const form2 = document.getElementById("form");
-const email2 = document.getElementById("email2");
-const password2 = document.getElementById("password-sign-in");
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  function checkInputs2() {
-    // trim to remove the whitespaces
-    const emailValue2 = email2.value.trim();
-    const passwordValue2 = password2.value.trim();
-
-    if (emailValue2 === "") {
-      setErrorFor(email2, "Email cannot be blank");
-    } else if (!isEmail(emailValue2)) {
-      setErrorFor(email2, "Not a valid email");
-    } else {
-      setSuccessFor(email2);
-      if (passwordValue2 === "") {
-        setErrorFor(password2, "Password cannot be blank");
-      } else {
-        setSuccessFor(password2);
-        e.currentTarget.submit();
-      }
-    }
-  }
-
-  checkInputs2();
-});
-
-function setErrorFor(input, message) {
-  const formControl = input.parentElement;
-  const small = formControl.querySelector("small");
-  formControl.className = "form-control error";
-  small.innerText = message;
-}
-
-function setSuccessFor(input) {
-  const formControl = input.parentElement;
-  formControl.className = "form-control";
-}
-
-function isEmail(email) {
-  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-    email
-  );
-}
+//function isEmail(email) {
+//  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+//    email
+//  );
+//}
 
 //const signUpButton = document.getElementById("signUp");
 //const signInButton = document.getElementById("signIn");
